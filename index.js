3fe819be1afa46cfba960f36086b891d088e6bb8
2reesa <arees1309@gmail.com>
2022-08-01 11:28:55 +0100

    Added role check for specific commands

--- index.js
+++ index.js
@@ -14,6 +14,22 @@ function ping(message) {
   return;
 }
 
+function roleTest(message) {
+  // this function checks a user has a specific role - used for restricting commands on Castle In The Mist server
+  if (message.member.roles.cache.some(role => role.name === 'Knights') || message.member.roles.cache.some(role => role.name === 'Squires') || message.member.roles.cache.some(role => role.name === 'Baroness')) {
+    console.log("User has the Knights role, the Squires role, or the Baroness role");
+    return true;
+  } else {
+    console.log("User does not have an applicable role");
+    return false;
+  }
+}
+
+function warnRole(message, command) {
+  output.mirror("Unfortunately, the command **" + command + "** is not available at this time.  It is currently restricted to Knights, Squires, or the Baroness.", message);
+  return;
+}
+
 client.on("messageCreate", function(message) {
   if (message.author.bot) return;
   if (!message.content.startsWith(prefix)) return;
@@ -24,19 +40,19 @@ client.on("messageCreate", function(message) {
 
   switch (command) {
     case 'categories':
-      categories.categoryList(message);
+      if (roleTest(message)) { categories.categoryList(message); } else { warnRole(message, 'categories); }
       break;
     case 'daily':
-      downtime.daily(message, args);
+      if (roleTest(message)) { downtime.daily(message, args); } else { warnRole(message, 'daily'); }
       break;
     case 'downtime':
-      downtime.downtime(message, args);
+      if (roleTest(message)) { downtime.downtime(message, args); } else { warnRole(message, 'downtime'); }
       break;
     case 'ping':
       ping(message);
       break;
    case 'ledger':
-     ledger.main(args);
+     if (roleTest(message)) { ledger.main(args); } else { warnRole(message, 'ledger'); }
      break;
    case 'griflet':
       output.mirror(output.help(), message);
