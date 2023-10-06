ec0c32ecda689b3ef410ec3f0207b7ef82c71bc2
2reesa <arees1309@gmail.com>
2023-10-06 08:54:41 +0100

    Improved prank command

--- commands/users/pingSpam.js
+++ commands/users/pingSpam.js
@@ -1,10 +1,11 @@
 const { SlashCommandBuilder } = require('discord.js'),
 	output = require('../snippets/output');
 
-async function loop(targetChannel, finalTarget, duration) {
+async function loop(targetChannel, finalTarget, duration, delay) {
 	for (i = 0; i < duration; i++) {
+		console.log("Attempt #" + i+1);
 		output.specificMirror(["Hi <@" + finalTarget + ">!"], Array.from(targetChannel.values())[0]); 
-		await new Promise(resolve => setTimeout(resolve, 15000));
+		await new Promise(resolve => setTimeout(resolve, delay * 1000)); // this sets a delay in miliseconds.  1 second = 1000.
 	}
 	return;
 }
@@ -14,13 +15,15 @@ async function gatherMaterials(interaction) {
 		channels = await interaction.guild.channels.fetch(),
 		target = interaction.options.getString('user'),
 		number = interaction.options.getString('number'),
-		targetChannel = channels.filter(channel => channel.name === "viewing-area");
-	console.log(targetChannel);
-	if (users.filter(member => member.user.username === target).size > 0) {
+		target_channel_name = interaction.options.getString('channel'),
+		delay = interaction.options.getString('delay')
+		targetChannel = channels.filter(channel => channel.name === target_channel_name);
+	if (users.filter(member => member.user.username === target).size > 0) { // triggers if Griflet found the user from the server member list
 		console.log("I have found the user to spam");
 		const finalTarget = users.filter(member => member.user.username === target);
-		console.log("Hi <@" + Array.from(finalTarget.keys())[0] + ">!");
-		loop(targetChannel, Array.from(finalTarget.keys())[0], number);
+		if (targertChannel != undefined) { // triggers if a channel that exists was entered
+			loop(targetChannel, Array.from(finalTarget.keys())[0], number, delay);
+		}
 	}
 	return;
 }
@@ -28,17 +31,27 @@ async function gatherMaterials(interaction) {
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('ping_spam')
-		.setDescription('Spam specific user with designated number of pings in viewing-area')
+		.setDescription('Spam specific user with designated number of pings')
 		.addStringOption(option =>
-			option.setName('user')
+			option.setName('user') // collect a username 
 				.setDescription('the user to spam')
 				.setRequired(true))
 		.addStringOption(option =>
-			option.setName('number')
+			option.setName('number') // set the maximum number of pings
 				.setDescription('the number of times to ping')
+				.setRequired(true))
+		.addStringOption(option =>
+			option.setName('channel') // set the channel to ping
+				.setDescription('the channel to send the pings to')
+				.setRequired(true))
+		.addStringOption(option =>
+			option.setName('delay') // set a delay between pings
+				.setDescription('the delay in seconds between each ping')
 				.setRequired(true)),
 	async execute(interaction) {
+		await interaction.deferReply();
+		interaction.editReply("Executing...");
 		gatherMaterials(interaction);
-		interaction.reply("Executing");
+		interaction.editReply("Execution Complete");
 	},
 };
