80436bc70caa346331eb28a32a9331944b0233b5
2reesa <arees1309@gmail.com>
2023-11-03 13:13:09 +0000

    Improved console output of downtimeSpend

--- commands/moderation/downtimeSpend.js
+++ commands/moderation/downtimeSpend.js
@@ -210,13 +210,16 @@ async function checkForLevelling(bladeData) {
 }
 
 async function prompter(timeLimit, interaction) { // this collects a user's reply after a prompt
+  console.log("Awaiting prompt response...");
   return new Promise (function (resolve, reject) {
   const collectorFilter = m => m.author.id == interaction.user.id,
     collector = interaction.channel.awaitMessages({ filter: collectorFilter, time: timeLimit, max: 1})
       .then(collected => {
+        console.log("Response received:");
         const key = Array.from(collected.keys());
         resolve(collected.get(key[0]).content);
-      }).catch(collected => {
+      }).catch(error => {
+      	console.error(error);
         resolve(null);
       });
   });
