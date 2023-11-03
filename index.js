0dc273a86365377e0077d8d23b905e07d86c9d87
2reesa <arees1309@gmail.com>
2023-11-03 14:57:49 +0000

    Fixed error in index.js that was preventing Griflet reading or receiving messages

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const fs = require('node:fs');
 const path = require('node:path');
 const {Client, Collection, Events, GatewayIntentBits } = require('discord.js');
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers] }); // Create a new client instance
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] }); // Create a new client instance
 client.cooldowns = new Collection();
 client.commands = new Collection(); // Load command files
 
