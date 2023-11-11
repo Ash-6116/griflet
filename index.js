cbdfb7087d217a9318cea5ae57371a93941e1de4
2reesa <arees1309@gmail.com>
2023-11-11 09:25:18 +0000

    Fixed index.js

--- index.js
+++ index.js
@@ -6,26 +6,25 @@ const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBit
 client.cooldowns = new Collection();
 client.commands = new Collection(); // Load command files
 client.on('messageCreate', async (message) => {
-         // 23.11 addition as a joke, wait for a message from viewing-area including the word 'morning'
-         if (message.channelId === '473284658025594881' && message.author.id != '897148749267222649') { // message >
-         //if (message.guildId === '775705450338451478' && message.author.id != '897148749267222649') { // message >
-                 if (message.content.toLowerCase().includes("morning")) { // if someone posts a message containing >
-                         // post "Morning user, how are you?"
-                         await message.reply("morning " + message.author.username + ", how are things with you?");
-                         console.log("greeting sent");
-                 } else if (message.content.toLowerCase().includes("how about you") || message.content.toLowerCase().includes("hbu") || message.content.toLowerCase().includes("how are you")) { // if someone posts a message containing
-                         // post "Pretty good thanks"
-                         await message.reply("I'm doing pretty good thanks.");
-                         console.log("response sent");
-                 }
-         }
-         // 23.11 addition as serious, wait for a message in ads-discussion with three keywords and auto pin it.
-         //if (message.guildId === '775705450338451478' && message.channelId === '1172549592345235466') { // on dev>
-         if (message.guildId === '473284658025594881' && message.channelId === '711238243676586023') {
-                 if (message.content.toLowerCase().includes("**title**") && message.content.toLowerCase().includes("**game**")) {
-                         message.pin(); // pin the message
-                 }
-         }
+	// 23.11 addition as a joke, wait for a message from viewing-area including the word 'morning'
+	if (message.channelId === '473284658025594881' && message.author.id != '897148749267222649') { // message
+	//if (message.guildId === '775705450338451478' && message.author.id != '897148749267222649') { // message
+		if (message.content.toLowerCase().includes("morning")) { // if someone posts a message containing
+			await message.reply("morning <@" + message.author.id + ">, how are things with you?"); // greet the user
+			console.log("greeting sent");
+		} else if (message.content.toLowerCase().includes("how about you") || message.content.toLowerCase().includes("hbu") || message.content.toLowerCase().includes("how are you")) { // if someone posts a message containing
+			await message.reply("I'm doing pretty good thanks."); // reply to the user
+		console.log("response sent");
+		}
+	}
+	// 23.11 addition as serious, wait for a message in ads-discussion with three keywords and auto pin it.
+	//if (message.guildId === '775705450338451478' && message.channelId === '1172549592345235466') { // on dev
+	if (message.guildId === '473284658025594881' && message.channelId === '711238243676586023') {
+		if (message.content.toLowerCase().includes("**title**") && message.content.toLowerCase().includes("**game**")) {
+		message.pin(); // pin the message
+		}
+	}
+	// TODO - Spin off to its own module!!!
 });
 
 
