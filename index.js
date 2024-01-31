eed6f6abb4504640dcfbb157cfcb57a06058fc65
2reesa <arees1309@gmail.com>
2024-01-31 07:05:27 +0000

    Fixed missing requirement of index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const fs = require('node:fs');
 const path = require('node:path');
-const {Client, Collection, Events, GatewayIntentBits } = require('discord.js');
+const {Client, Collection, Events, GatewayIntentBits, Partials } = require('discord.js');
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent], partials: [ Partials.GuildMember ] }); // Create a new client instance
 client.cooldowns = new Collection();
