a49636a1fce533a29e243d61f8559e35232d2780
2reesa <arees1309@gmail.com>
2024-04-02 12:55:14 +0100

    added new GuildMember event files

---
+++ events/eventsGuildMemberAdd.js
@@ -0,0 +1,16 @@
+const { Events, EmbedBuilder } = require('discord.js');
+
+module.exports = {
+	name: Events.GuildMemberAdd,
+	async execute(member) {
+		const channels = await member.guild.channels.fetch(),
+			embed = new EmbedBuilder()
+				.setTitle(member.user.username + " has joined the server!")
+				.setImage(member.user.displayAvatarURL())
+				.addFields({name: "username", value: member.user.username, inline: false});
+		if (member.user.has(globalName)) {
+			embed.addFields({name: "nickname", value: member.user.globalName, inline: false});
+		}
+		channels.find(channel => channel.name === "bot-stuff").send({ embeds: [embed] });
+	}
+}
---
+++ events/eventsGuildMemberUpdate.js
@@ -0,0 +1,18 @@
+const { Events, EmbedBuilder } = require('discord.js');
+
+module.exports = {
+	name: Events.GuildMemberUpdate,
+	async execute(member) {
+		if (member.pending != false) {
+			const channels = await member.guild.channels.fetch(),
+				embed = new EmbedBuilder()
+					.setTitle(member.user.username + " has accepted the server rules!")
+					.setImage(member.user.displayAvatarURL())
+					.addFields({name: "username", value: member.user.username, inline: false});
+			if (member.user.has(globalName)) {
+				embed.addFields({name: "nickname", value: member.user.globalName, inline: false});
+			}
+			channels.find(channel => channel.name === "bot-stuff").send({ embeds: [embed] });
+		}
+	}
+}
