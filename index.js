641a20f18bed6db2fe301bda0b2a8e68b9e8e1ee
2reesa <arees1309@gmail.com>
2025-03-03 11:54:41 +0000

    Reformatted index.js so that it can be used in both RELEASE and DEBUG modes using the -d flag (originally the two were separate files)

--- index.js
+++ index.js
@@ -1,21 +1,33 @@
-const fs = require('node:fs');
-const path = require('node:path');
-const {Client, Collection, Events, GatewayIntentBits, Partials } = require('discord.js');
+// Check for debug and if so, bring in the process.env options locally
+if (process.argv.indexOf("-d") > -1) {
+	require('dotenv').config();
+}
 
-const client = new Client({ intents: [ GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent, GatewayIntentBits.GuildMessageReactions], partials: [Partials.GuildMember, Partials.Channel, Partials.Message, Partials.Reaction], }); // Create a new client instance
+if (process.argv.indexOf("-t") > -1) {
+	// Set certain variables to their test counterparts
+	console.log("Original sheet:" + process.env.spreadsheetId);
+	process.env.spreadsheetId = process.env.testSpreadsheetId;
+	console.log("Altered sheet:" + process.env.spreadsheetId);
+}
+
+const fs = require('node:fs'),
+	path = require('node:path'),
+	foldersPath = path.join(__dirname, 'commands'),
+	commandFolders = fs.readdirSync(foldersPath),
+	eventsPath = path.join(__dirname, 'events'),
+	eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js')),
+	{Client, Collection, Events, GatewayIntentBits, Partials } = require('discord.js'),
+	client = new Client({ intents: [ GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent, GatewayIntentBits.GuildMessageReactions], partials: [Partials.GuildMember, Partials.Channel, Partials.Message, Partials.Reaction], }); // Create a new client instance
 client.cooldowns = new Collection();
 client.commands = new Collection(); // Load command files
 
-const foldersPath = path.join(__dirname, 'commands');
-const commandFolders = fs.readdirSync(foldersPath);
-
 for (const folder of commandFolders) {
-  const commandsPath = path.join(foldersPath, folder);
-  const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+  const commandsPath = path.join(foldersPath, folder),
+  	commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 
   for (const file of commandFiles) {
-    const filePath = path.join(commandsPath, file);
-    const command = require(filePath);
+    const filePath = path.join(commandsPath, file),
+    	command = require(filePath);
     // Set a new item in the Collection with the key as the command name and the value as the exported module
     if ('data' in command && 'execute' in command) {
       client.commands.set(command.data.name, command);
@@ -25,12 +37,9 @@ for (const folder of commandFolders) {
   }
 }
 
-const eventsPath = path.join(__dirname, 'events');
-const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
-
 for (const file of eventFiles) {
-  const filePath = path.join(eventsPath, file);
-  const event = require(filePath);
+  const filePath = path.join(eventsPath, file),
+  	event = require(filePath);
   if (event.once) {
     client.once(event.name, (...args) => event.execute(...args));
   } else {
