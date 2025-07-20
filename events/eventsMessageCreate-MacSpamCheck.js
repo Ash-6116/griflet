611d85319ca58c19c61577d6e4491bc85cdcc19c
ash6116 <cysgodsoftware@gmail.com>
2025-07-20 10:12:58 +0100

    Bug fixes for mac spam detector

--- events/eventsMessageCreate-MacSpamCheck.js
+++ events/eventsMessageCreate-MacSpamCheck.js
@@ -7,7 +7,7 @@ module.exports = {
 				if (message.content.toLowerCase().includes("macbook") && message.content.toLowerCase().includes("sell")) {
 					// Send an alert to bot-spam with a link to the original message tagged for the Knights
 					const guildChannels = await message.guild.channels.fetch(), guildRoles = await message.guild.roles.fetch();
-					guildChannels.find(channel => channel.name === "bot-spam").send("Possible macbook spam detected <@" + guildRoles.find(role => role.name === 'Knights').id + "> Guild:" + message.guild.id + " Channel:" + message.channel.id  + "Message: " + message.id);
+					guildChannels.find(channel => channel.name === "bot-stuff").send("<@&" + guildRoles.find(role => role.name === 'Knights').id + ">\nSuspected macbook spam detected, please investigate\nhttps://discord.com/channels/" + message.guild.id + "/" + message.channel.id + "/" + message.id);
 					return;
 				}
 		}
