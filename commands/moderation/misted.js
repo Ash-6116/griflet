598cf3a3e7c2651331deea479da7f40f8ec5f54a
Ash-6116 <arees1309@gmail.com>
2026-01-28 16:10:25 +0000

    improved misted and macspamcheck to enable reactions output in misted and making the spam check more sensitive

--- commands/moderation/misted.js
+++ commands/moderation/misted.js
@@ -20,41 +20,74 @@ async function find_member(dataset, misted_user) {
 	return matches;
 }
 
-async function misted(misted_user, roster) {
-	// First, check the roster to see if they have a sheet in progress
-	const result_from_roster = await roster_check(roster, misted_user);
-	// Second, check the Guild Roster | Active tab
-	const searchActive = await find_member(await getAllSpreadsheetValues(process.env.spreadsheetId, "Roster"), misted_user);
-	// Third, check the Guild Roster | Inactive tab
-	const searchInactive = await find_member(await getAllSpreadsheetValues(process.env.spreadsheetId, "Inactive"), misted_user);
+async function misted(misted_user, roster, questBoard) {
+	let outputMap = new Map();
+	outputMap.set("roster", await roster_check(roster, misted_user)); // First, check the roster to see if they have a sheet in progress
+	outputMap.set("active", await find_member(await getAllSpreadsheetValues(process.env.spreadsheetId, "Roster"), misted_user)); // Second, check the Guild Roster | Active tab
+	outputMap.set("inactive", await find_member(await getAllSpreadsheetValues(process.env.spreadsheetId, "Inactive"), misted_user)); // Third, check the Guild Roster | Inactive tab
 	// Fourth, check the quest-board for any reactions from the user
-	// TODO
-	return [result_from_roster, searchActive, searchInactive];
+	// TODO - remember that misted_user is a USERNAME string, not a USER object
+	const questKey = Array.from(questBoard.keys());
+	let reacted = []
+	for (k = 0; k < questKey.length; k++) {
+		const quests = await questBoard.get(questKey[k]).messages.fetch(),
+			questsID = Array.from(quests.keys());
+		for (q = 0; q < questsID.length; q++) {
+			const reactions = quests.get(questsID[q]).reactions.cache;
+			if (reactions.size > 0) { // really only care about these as they have reactions data
+				const reactionsKeys = Array.from(reactions.keys());
+				for (r = 0; r < reactionsKeys.length; r++) {
+					let reaction = reactions.get(reactionsKeys[r]),
+						users = await reaction.users.fetch(); // if this contains our misted_user, then we want to flash it!!!
+					users.forEach(user => { // don't need a for loop as we're not awaiting data ;)
+						if (user.username === misted_user) {
+							reacted.push(quests.get(questsID[q]).content.replace('-\n', '').replace(/\-/g,'').split('\n')[0]); // this nonsense gets just the title from the message.content
+						}
+					});
+				}
+			}
+		}
+	}
+	outputMap.set("reactions", reacted);
+	return outputMap;
 }
 
-function processOutput(misted_returned) {
-	// expecting a three item array, first being a boolean, then second and third being row data from the Roster and Inactive sheets in the Guild Roster
+function processOutput(misted_returned) { // expecting a map, with a roster boolean, active and inactive roster fetches, and a reactions arra
 	let outputString = "";
-	if (misted_returned[0]) {
-		outputString += "-  User left a message in roster\n";
-	} else {
-		outputString += "-  User has not left a message in roster\n";
+	if (misted_returned.has("roster")) { // misted_returned should ALWAYS have this, but an error check hurts nobody
+		if (misted_returned.get("roster")) { // this works as result_from_roster is a boolean
+			outputString += "- User left a message in roster\n";
+		} else {
+			outputString += "- User has not left a message in roster\n";
+		}
 	}
-	if (misted_returned[1].length > 0) {
-		outputString += "-  User has the following Blade(s) on the Active roster:\n";
-		misted_returned[1].forEach(blade => {
-			outputString += "     " +  blade[2];
-			if (blade[13] != "") { // checking status in case they were on a quest
-				outputString += ", they were on the quest: " + blade[13];
-			}
-			outputString += "\n";
-		});
+	if (misted_returned.has("active")) {
+		if (misted_returned.get("active").length > 0) {
+			outputString += "- User has the following Blade(s) on the Active roster:\n";
+			misted_returned.get("active").forEach(blade => {
+				outputString += "     " + blade[2];
+				if (blade[13] != "") { // checking status in case they were on a quest
+					outputString += ", they were on the quest: " + blade[13];
+				}
+				outputString += "\n";
+			});
+		}
 	}
-	if (misted_returned[2].length > 0) {
-		outputString += "-  User has the following Blade(s) on the Inactive roster:\n";
-		misted_returned[2].forEach(blade => {
-			outputString += "    " + blade[2] + "\n";
-		});
+	if (misted_returned.has("inactive")) {
+		if (misted_returned.get("inactive").length > 0) {
+			outputString += "- User has the following Blade(s) on the Inactive roster:\n";
+			misted_returned.get("inactive").forEach(blade => {
+				outputString += "     " + blade[2] + "\n";
+			});
+		}
+	}
+	if (misted_returned.has("reactions")) {
+		if (misted_returned.get("reactions").length > 0) {
+			outputString += "- User has left the following reactions in quest-board:\n";
+			misted_returned.get("reactions").forEach(reaction => {
+				outputString += "     " + reaction + "\n";
+			});
+		}
 	}
 	return outputString;
 }
@@ -73,7 +106,7 @@ module.exports = {
 			const outputEmbed = new EmbedBuilder()
 				.setTitle("Result of check on " + interaction.options.getString('user'))
 			const channels = await interaction.guild.channels.fetch();
-			const result_of_checks = await misted(interaction.options.getString('user'), channels.filter(channel => channel.name === "roster"));
+			const result_of_checks = await misted(interaction.options.getString('user'), channels.filter(channel => channel.name === "roster"), channels.filter(channel => channel.name === "quest-board"));
 			outputEmbed.setDescription(processOutput(result_of_checks));
 			interaction.editReply({ embeds: [outputEmbed] });
 			//interaction.editReply("__**Result of check on " + interaction.options.getString('user') + "**__\n" + processOutput(result_of_checks));
--- events/eventsMessageCreate-MacSpamCheck.js
+++ events/eventsMessageCreate-MacSpamCheck.js
@@ -4,7 +4,8 @@ module.exports = {
 	name: Events.MessageCreate,
 	async execute(message) {
 		if (message.guild.name.includes("Griflet Development Server") || message.guild.name.includes("A Castle in the Mist")) {
-				if (message.content.toLowerCase().includes("macbook") && message.content.toLowerCase().includes("sell")) {
+				const parsedMessage = message.content.toLowerCase();
+				if ((parsedMessage.includes("mac") || parsedMessage.includes("camera")) && (parsedMessage.includes("sell") || parsedMessage.includes("sale"))) {
 					// Send an alert to bot-spam with a link to the original message tagged for the Knights
 					const guildChannels = await message.guild.channels.fetch(), guildRoles = await message.guild.roles.fetch();
 					guildChannels.find(channel => channel.name === "bot-stuff").send("<@&" + guildRoles.find(role => role.name === 'Knights').id + ">\nSuspected macbook spam detected, please investigate\nhttps://discord.com/channels/" + message.guild.id + "/" + message.channel.id + "/" + message.id);
