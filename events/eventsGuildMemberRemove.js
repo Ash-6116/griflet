157cce5e7e0190dd46b05aa05ebddf1a2a150049
Ash-6116 <arees1309@gmail.com>
2026-01-29 09:28:44 +0000

    Fixed typo in eventsGuildMemberRemove.js

--- events/eventsGuildMemberRemove.js
+++ events/eventsGuildMemberRemove.js
@@ -11,7 +11,7 @@ module.exports = {
 		const channels = await member.guild.channels.fetch();
 		console.log(member.user.username + " has left the server!");
 		console.log(member);
-		const result_of_checks = await misted(member.user.username, channels.filter(channel => channel.name === "roster"), channels.filter(channel => channel.name === "quest-board");
+		const result_of_checks = await misted(member.user.username, channels.filter(channel => channel.name === "roster"), channels.filter(channel => channel.name === "quest-board"));
 		embed.setDescription(processOutput(result_of_checks));
 		embed.setThumbnail(member.user.displayAvatarURL());
 		channels.find(channel => channel.name === "bot-stuff").send({ embeds: [embed] });
