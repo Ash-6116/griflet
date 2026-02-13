d538271cf3086bd22e3573ffd3f32590e233156a
Ash-6116 <arees1309@gmail.com>
2026-02-13 18:24:30 +0000

    Update eventsMessageCreate-MacSpamCheck.js
    Added giving and DM to list of words to check for due to recent suspicious messages on server

--- events/eventsMessageCreate-MacSpamCheck.js
+++ events/eventsMessageCreate-MacSpamCheck.js
@@ -5,7 +5,7 @@ module.exports = {
 	async execute(message) {
 		if (message.guild.name.includes("Griflet Development Server") || message.guild.name.includes("A Castle in the Mist")) {
 				const parsedMessage = message.content.toLowerCase();
-				if ((parsedMessage.includes("mac") || parsedMessage.includes("camera")) && (parsedMessage.includes("sell") || parsedMessage.includes("sale"))) {
+				if ((parsedMessage.includes("mac") || parsedMessage.includes("camera")) && (parsedMessage.includes("sell") || parsedMessage.includes("sale") || parsedMessage.includes("giving") || parsedMessage.includes("DM"))) {
 					// Send an alert to bot-spam with a link to the original message tagged for the Knights
 					const guildChannels = await message.guild.channels.fetch(), guildRoles = await message.guild.roles.fetch();
 					guildChannels.find(channel => channel.name === "bot-stuff").send("<@&" + guildRoles.find(role => role.name === 'Knights').id + ">\nSuspected macbook spam detected, please investigate\nhttps://discord.com/channels/" + message.guild.id + "/" + message.channel.id + "/" + message.id);
