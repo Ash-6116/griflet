2ea1b6f3704bcf73e6950462374c12851f0f9bf5
Ash-6116 <arees1309@gmail.com>
2026-09-02 08:58:21 +0100

    Adjusted returning Blade ping to send to new channel and tag the knights and squires

--- events/eventsMessageCreate-CheckInactiveRoster.js
+++ events/eventsMessageCreate-CheckInactiveRoster.js
@@ -31,15 +31,16 @@ module.exports = {
 		const result_of_checks = await find_member(await getAllSpreadsheetValues(process.env.spreadsheetId,  "Inactive"), message.author.username);
 		if (result_of_checks.length > 0) {
 			const guildChannels = await message.guild.channels.fetch(),
+				guildRoles = await message.guild.roles.fetch(),
 				postChannel = guildChannels.get(message.channelId).name,
 				bladeChannels = ["viewing-area", "dice-tray", "blades-confessionals", "rec-room", "guild-hall", "courtyard", "sunroom", "shoppe", "library", "strathmore", "quest-caravan-1", "qc1-ooc", "quest-caravan-2", "qc2-ooc", "quest-caravan-3", "qc3-ooc", "quest-caravan-4", "qc4-ooc", "quest-caravan-5", "qc5-ooc", "quest-caravan-6", "qc6-ooc", "quest-caravan-7", "qc7-ooc", "quest-caravan-8", "qc8-ooc", "quest-caravan-9", "qc9-ooc", "quest-caravan-10", "qc10-ooc"];
 			if (bladeChannels.includes(postChannel)) { // if the post was made in a blade channel, returns true
 				let output = "Please move the following Blade from the __**Inactive Roster**__ to the __**Active Roster**__ as their player has returned to the server:";
 				if (outputStyle == "Legacy") {
-					guildChannels.find(channel => channel.name === "bot-stuff").send(legacyOutput(output, result_of_checks, message));
+					guildChannels.find(channel => channel.name === "returning-blade-alerts").send("<@&" + guildRoles.find(role => role.name === "Knights").id + ">, <@&" + guildRoles.find(role => role.name === "Squires").id + ">" + legacyOutput(output, result_of_checks, message));
 				} else if (outputStyle == "Embed") {
 					const toSend = await embedOutput(output, result_of_checks, message);
-					guildChannels.find(channel => channel.name === "bot-stuff").send({embeds: [toSend] });
+					guildChannels.find(channel => channel.name === "returning-blade-alerts").send({ content: "<@&" + guildRoles.find(role => role.name === "Knights").id + ">, <@&" + guildRoles.find(role => role.name === "Squires").id + ">", embeds: [toSend] });
 				} else {
 					console.log("Improper output style selected, must be either Legacy or Embed!!!");
 				}
